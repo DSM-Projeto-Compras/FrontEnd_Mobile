c08026a170d413c5a569139471c2d019c60be9c0
Eduardo Brando Almeida <dugu2005@gmail.com>
2025-05-27 20:24:50 -0300

    fix/Paper provider no App

--- App.js
+++ App.js
@@ -1,36 +1,44 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/login';
-import RegisterScreen from './screens/cadastro';
-import OrderScreen from './screens/pedido';
-import { NavigationContainer } from '@react-navigation/native';
-import { PaperProvider } from 'react-native-paper';
-import  customTheme  from './tema'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/login";
+import RegisterScreen from "./screens/cadastro";
+import OrderScreen from "./screens/pedido";
+import HistScreen from "./screens/historico";
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./contexts/AuthContext";
+import { PaperProvider } from "react-native-paper";
+import customTheme from "./tema";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <PaperProvider theme={customTheme}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen 
-            name="Register"
-            component={RegisterScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen 
-            name="Order"
-            component={OrderScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AuthProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Register"
+              component={RegisterScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Order"
+              component={OrderScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Hist"
+              component={HistScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthProvider>
     </PaperProvider>
   );
 }
-
