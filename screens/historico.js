bdbb85efaa883ecfc11bea49f32a6b05572f954f
Eduardo Brando Almeida <dugu2005@gmail.com>
2025-05-28 16:12:04 -0300

    refactor/Adicionando mais card

--- screens/historico.js
+++ screens/historico.js
@@ -1,32 +1,48 @@
 import React from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
 import Header from "../components/header";
 import BottomNav from "../components/bottomNavigation";
 import OrderCard from "../components/card"
 
 const HistScreen =()=>{ 
-    return(
-        <View style={styles.pageContainer}>
-             <ScrollView contentContainerStyle={styles.scrollContent}>
-                <Text variant="headlineMedium" style={styles.title}>
-                  Histórico
-                </Text>
-                 <Header /> 
-                 <OrderCard 
-                 itemName={'Caneta'}
-                 quantity={4}
-                 orderDate={'10/04/2025'}
-                 />
-             </ScrollView>
-         <View style={styles.bottomNavContainer}>
-             <BottomNav />
-         </View>
-        </View>
-    );
+  return(
+    <View style={styles.pageContainer}>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <Header />
+        <Text variant="headlineMedium" style={styles.title}>
+          Histórico
+        </Text> 
+        <OrderCard 
+          itemName={'Caneta'}
+          quantity={4}
+          orderDate={'10/04/2025'}
+        />
+        <OrderCard 
+          itemName={'Caixa de Giz de Cera'}
+          quantity={2}
+          orderDate={'18/02/2025'}
+        />
+        <OrderCard 
+          itemName={'Pacote de Pregos'}
+          quantity={1}
+          orderDate={'04/05/2025'}
+        />
+        <OrderCard 
+          itemName={'Chapa de MDF 15mm'}
+          quantity={15}
+          orderDate={'28/02/2025'}
+        />
+      </ScrollView>
+      <View style={styles.bottomNavContainer}>
+        <BottomNav />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    pageContainer: {
+  pageContainer: {
     flex: 1,
   },
   scrollContent: {
