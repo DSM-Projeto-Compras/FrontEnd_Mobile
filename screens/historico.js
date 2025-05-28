cce983cb1a9125b56768d07301d176ed334b9378
Paulo <paulocesarvictorio940@gmail.com>
2025-05-27 21:37:14 -0300

    first commit

--- screens/historico.js
+++ screens/historico.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import Header from "../components/header";
 import BottomNav from "../components/bottomNavigation";
 import BtnPadrao from "../components/button";
 
 const HistScreen =()=>{ 
     return(
-        <View>
-                <ScrollView>
-                    <Header />
-                </ScrollView>
-            <View style={styles.bottomNavContainer}>
-                <BottomNav />
-            </View>
+        <View style={styles.pageContainer}>
+             <ScrollView contentContainerStyle={styles.scrollContent}>
+                 <Header /> 
+             </ScrollView>
+         <View style={styles.bottomNavContainer}>
+             <BottomNav />
+         </View>
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    pageContainer: {
+    flex: 1,
+  },
+  scrollContent: {
+    paddingBottom: 80,
+  },
+  formContainer: {
+    padding: 16,
+  },
+  title: {
+    marginBottom: 32,
+    textAlign: 'center',
+  },
+  input: {
+    marginBottom: 16,
+  },
+  bottomNavContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
 
 export default HistScreen;
